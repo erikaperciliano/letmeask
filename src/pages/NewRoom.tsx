b1fe8cd5ab1709b00e9cf5955a4f89ab4f8a89cf
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-12-16 18:27:10 -0300

    add NewRoom

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,36 @@
+import IllustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+
+
+import '../styles/auth.scss';
+
+export function NewRoom() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={IllustrationImg} alt="Illustration symbolizing questions and answers" />
+                <strong>Create Q&amp; Live</strong>
+                <p>Ask your audience's questions in real-time</p>
+            </aside>
+            <main>
+                <div className='main-content'>
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Create a new room</h2>
+                    <form>
+                        <input 
+                            type="text"
+                            placeholder='Room name'
+                        />
+                        <Button type="submit">
+                            Create room
+                        </Button>
+                    </form>
+                    <p>Want to join an existing room? <a href="#">click here</a></p>
+                </div>
+            </main>
+        </div>
+    )
+}
