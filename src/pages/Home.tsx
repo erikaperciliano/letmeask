e24f9238f3dd986f2505ce4504e23b45f1000edf
erikaperciliano <erikaperciliano.developer@gmail.com>
2021-12-15 21:56:52 -0300

    add styles at Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,33 +2,34 @@ import IllustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
 
 export function Home() {
     return (
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={IllustrationImg} alt="Illustration symbolizing questions and answers" />
                 <strong>Create Q&amp; Live</strong>
                 <p>Ask your audience's questions in real-time</p>
             </aside>
             <main>
-                <div>
+                <div className='main-content'>
                     <img src={logoImg} alt="Letmeask" />
-                </div>
-                <button>
-                    <img src={googleIconImg} alt="google´s logo" />
-                    Create your room on google
-                </button>
-                <div>or enter a room</div>
-                <form>
-                    <input 
-                        type="text"
-                        placeholder='Enter a room code'
-                    />
-                    <button type="submit">
-                        Enter a room
+                    <button className='create-room'>
+                        <img src={googleIconImg} alt="google´s logo" />
+                        Create your room on google
                     </button>
-                </form>
+                    <div className='separator'>or enter a room</div>
+                    <form>
+                        <input 
+                            type="text"
+                            placeholder='Enter a room code'
+                        />
+                        <button type="submit">
+                            Enter a room
+                        </button>
+                    </form>
+                </div>
             </main>
         </div>
     )
