c6bb2f9a63c8dc68d9ea2c2580f49c1ebba5f221
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-12-17 18:26:41 -0300

    Remove NewRoom and add Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import { Home } from './pages/Home';
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <NewRoom/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms/new" element={<NewRoom />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
