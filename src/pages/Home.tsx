04528623c1f485d8830f1dab8474bd89b734b35b
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-12-20 20:33:40 -0300

    saving user information with React Context API
    - release user access to the NewsRoom page only when the user is logged in
    
    - share user information between multiple components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,31 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '../components/Button';
+import { TestContext } from '../App';
+
+import { auth, firebase } from '../services/firebase';
+
 import IllustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
-import { Button } from '../components/Button';
 
-import { auth, firebase } from '../services/firebase';
 
 import '../styles/auth.scss';
 
 export function Home() {
-    // const history = useNavigate();
+    const history = useNavigate();
+    const value = useContext(TestContext);
 
     function navigateToNewRoom () {
         const provider = new firebase.auth.GoogleAuthProvider();
 
         auth.signInWithPopup(provider).then(result => {
             console.log('result: ', result);
+            
+            history('/rooms/new');
         })
-
-        // history('/rooms/new');
     }
 
     return (
@@ -29,6 +36,7 @@ export function Home() {
                 <p>Ask your audience's questions in real-time</p>
             </aside>
             <main>
+                <h1>{value}</h1>
                 <div className='main-content'>
                     <img src={logoImg} alt="Letmeask" />
                     <button onClick={navigateToNewRoom} className='create-room'>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,6 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { TestContext } from '../App';
 
 import IllustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -6,9 +8,12 @@ import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 
 
+
 import '../styles/auth.scss';
 
 export function NewRoom() {
+    const value = useContext(TestContext);
+
     return (
         <div id="page-auth">
             <aside>
@@ -17,6 +22,7 @@ export function NewRoom() {
                 <p>Ask your audience's questions in real-time</p>
             </aside>
             <main>
+                <h1>{value}</h1>
                 <div className='main-content'>
                     <img src={logoImg} alt="Letmeask" />
                     <h2>Create a new room</h2>
