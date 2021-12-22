7cd4ce3dd9ebf4251edfe47984d1da641dbf53e3
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-12-22 18:00:03 -0300

    removing context information and call it here

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import {
   BrowserRouter,
@@ -8,76 +8,18 @@ import {
 
 import { Home } from './pages/Home';
 import { NewRoom } from "./pages/NewRoom";
-import { auth, firebase } from './services/firebase';
 
-type User = {
-  id: string;
-  name: string;
-  avatar: string;
-}
-
-type AuthContextType = {
-  user: User | undefined;
-  signInWithGoogle: () => Promise<void>;
-}
-
-
-export const AuthContext = createContext({} as AuthContextType);
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
-  const [user, setUser] = useState<User>();
-
-  useEffect(() => {
-    //checks at firebase if exist user
-    const unsubscribe =  auth.onAuthStateChanged(user => {
-      if(user) {
-        const { displayName, photoURL, uid } = user;
-
-        if(!displayName || !photoURL) {
-          throw new Error('Missing information from Google Account.');
-        }
-  
-        setUser({
-          id: uid,
-          name: displayName,
-          avatar: photoURL 
-        })
-      }
-    })
-
-    return () => {
-      unsubscribe();
-    }
-  },[])
-
-  async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    const result = await auth.signInWithPopup(provider);
-
-    if(result.user) {
-      const { displayName, photoURL, uid } = result.user;
-
-      if(!displayName || !photoURL) {
-        throw new Error('Missing information from Google Account.');
-      }
-
-      setUser({
-        id: uid,
-        name: displayName,
-        avatar: photoURL 
-      })
-    }
-  }
-
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      <AuthContextProvider>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms/new" element={<NewRoom />} />
         </Routes>
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
