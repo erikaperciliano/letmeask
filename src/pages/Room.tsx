8e11313591b53d6d2823c094d8706a1a4e9b8ac2
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-12-23 15:55:53 -0300

    add Room Page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,35 @@
+import logoImage from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+
+import '../styles/room.scss';
+
 export function Room() {
     return(
-        <h1>Sala</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImage} alt="Letmeask" />
+                    <div>codigo</div>
+                </div>
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea
+                        placeholder='What do you want to ask?'
+                    />
+
+                    <div className="form-footer">
+                        <span>To submit a question, <button>please login</button>.</span>
+                        <Button type='submit'>Send question</Button>
+                    </div>
+                </form>
+            </main>
+        </div>
     );
 }
