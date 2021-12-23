7715a19b9d82f0af586371275369e005beff964f
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-12-23 08:41:30 -0300

    add function handleJoinRoom
    - Verifying if rooms exist

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,8 @@
 import { useNavigate } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 import { Button } from '../components/Button';
 
@@ -6,14 +10,13 @@ import IllustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
-
-
 import '../styles/auth.scss';
-import { useAuth } from '../hooks/useAuth';
+
 
 export function Home() {
     const history = useNavigate();
     const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
     async function navigateToNewRoom () {
         if(!user) {
@@ -21,6 +24,23 @@ export function Home() {
         }
 
         history('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if(roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()) {
+            alert('Room does´not exists.');
+            return;
+        }
+
+        history(`/rooms/${roomCode}`);
     }
 
     return (
@@ -38,10 +58,12 @@ export function Home() {
                         Create your room on google
                     </button>
                     <div className='separator'>or enter a room</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                             type="text"
                             placeholder='Enter a room code'
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Enter a room
