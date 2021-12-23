5d13a8dbef3fd9a8179e1c19e911096b9732b336
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-12-23 08:21:02 -0300

    redirecting user after create a new Room

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FormEvent, useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 
@@ -14,6 +14,7 @@ import { database } from '../services/firebase';
 export function NewRoom() {
     const { user } = useAuth();
     const [newRoom, setNewRoom] = useState('');
+    const history = useNavigate();
 
     async function handleCreateRoom(event: FormEvent) {
         event.preventDefault();
@@ -28,6 +29,8 @@ export function NewRoom() {
             title: newRoom,
             authorId: user?.id
         })
+
+        history(`/rooms/${firebaseRoom.key}`);
     }
 
     return (
